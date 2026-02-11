38ef99918fbb6abb2f8da2ac7bdff6e7c0843c6c
Gonçalo_Rodrigues <G.dRCPRO@protonmail.com>
2026-02-11 10:42:59 +0000

    Update look
    - Added animations to the content and project list;
    - Modified colors of components.

--- src/info.js
+++ src/info.js
@@ -2,9 +2,9 @@ import CodeWallpaper from "./assets/generalPhotos/CodeWallpaper.jpg"
 
 
 const informations = [
-  {name: 'AboutMe', information: "blablabla", image: CodeWallpaper},
-  {name: "SnakeGame", information: "This repository contains a simple implementation of the classic Snake game using the Python turtle module. The player controls a snake, navigating it to eat food and grow longer while avoiding collisions with the walls and its own tail.", image: CodeWallpaper},
-  {name: "8PuzzleSolver", information: "definition bla bla", image: CodeWallpaper}
+  {name: 'About Me', information: "blablabla", image: CodeWallpaper},
+  {name: "Snake Game", information: "This repository contains a simple implementation of the classic Snake game using the Python turtle module. The player controls a snake, navigating it to eat food and grow longer while avoiding collisions with the walls and its own tail.", image: CodeWallpaper},
+  {name: "8-Puzzle Solver", information: "definition bla bla", image: CodeWallpaper}
 ];
 
 export default informations;
