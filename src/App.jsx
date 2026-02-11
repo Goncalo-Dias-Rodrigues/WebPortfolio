3a85c156fe0dd976a925dcdf607500a2c1c79785
Gonçalo_Rodrigues <G.dRCPRO@protonmail.com>
2026-02-11 09:55:20 +0000

    Added Sidebar
    - Added dropdown;
    - Added letter outline;
    - Added placeholde info.

--- src/App.jsx
+++ src/App.jsx
@@ -3,49 +3,49 @@ import './App.css'
 import informations from './info.js'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [content, setContent] = useState("")
+  const [showProjects, setShowProjects] = useState(false);
+
 
   return (
     <>
       <div className='top-bar'>
-        <h2>
+        <h2 className='outlined-text'>
           Gonçalo Dias Rodrigues
         </h2> 
       </div>
 
       <div className='side-bar'>
-        <button onClick={() => setContent(informations[0])}>
+        <button className='fixed-button' onClick={() => setContent(informations[0])}>
           👉 About Me 👈
         </button>
+
+        <button className= 'fixed-button' style={{top: '160px'}}    onClick={() => setShowProjects(!showProjects)}>
+          {showProjects ? "👇 Projects 👇" : "👉 Projects 👈"}
+        </button>
+
+        {showProjects && (<div>
+          {informations.slice(1).map((item, index) => (
+            <button className='projects-list'
+              key={index} 
+              onClick={() => setContent(item)}>
+              {item.name}
+            </button>
+          ))}
+        </div>)}
 
         {
           content && (
             <div className='content-box'>
-              <h2>{content.name}</h2>
-              <p>{content.information}</p>
+              <h2 className='outlined-text'>{content.name}</h2>
+              <p className='outlined-text'>{content.information}</p>
+              <img src={content.image} alt = {content.name} className='content-box-image' ></img>
               <button onClick={() => setContent(null)}>Fechar</button>
             </div>
           )
         }
-
-
-
-
-
-        <h2>
-          Gonçalo Dias Rodrigues
-        </h2> 
       </div>
-
-
-
-      
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-      
-      
+    
     </>
   )
 }
--- src/info.js
+++ src/info.js
@@ -2,7 +2,9 @@ import CodeWallpaper from "./assets/generalPhotos/CodeWallpaper.jpg"
 
 
 const informations = [
-  { name: 'AboutMe', information: "blablabla", imagem: CodeWallpaper}
+  {name: 'AboutMe', information: "blablabla", image: CodeWallpaper},
+  {name: "SnakeGame", information: "This repository contains a simple implementation of the classic Snake game using the Python turtle module. The player controls a snake, navigating it to eat food and grow longer while avoiding collisions with the walls and its own tail.", image: CodeWallpaper},
+  {name: "8PuzzleSolver", information: "definition bla bla", image: CodeWallpaper}
 ];
 
 export default informations;
